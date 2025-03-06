b31ce77e59744d2cd3efd2a26b8bd78a47be5468
byteauthority <154011817+byteauthority@users.noreply.github.com>
2025-03-06 18:21:54 +0100

    md-shown-properly

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -15,7 +17,8 @@ def create(request):
     })
 
 def open(request, title):
-    page = util.get_entry(title)
+    page = markdown2.markdown(util.get_entry(title))
+
     return render(request, "encyclopedia/page.html", {
         "page": page,
         "title": title,
